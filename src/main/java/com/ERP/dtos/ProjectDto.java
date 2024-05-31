faacb3af3af6b74033740173016be27d6ab4ebb6
simran2519 <simranjit.kaur@avisoft.io>
2024-05-31 11:05:39 +0530

    updated project dto

--- src/main/java/com/ERP/dtos/ProjectDto.java
+++ src/main/java/com/ERP/dtos/ProjectDto.java
@@ -19,11 +19,11 @@ public class ProjectDto
 {
     private long projectId;
     @NotNull(message = "Name of Project cannot be null")
-    @Size(min=1,max = 50,message = "min characters are 3 and maximum characters can be upto 50")
+    @Size(min=1,max = 50,message = "min characters are 1 and maximum characters can be upto 50")
     @Pattern(regexp = "^[^\\s].*$", message = "Name of project cannot be empty")
     private String name;
     @NotNull(message = "Description cannot be empty")
-    @Size(min=1,max = 50,message = "min characters are 3 and maximum characters can be upto 50")
+    @Size(min=1,max = 200,message = "min characters are 3 and maximum characters can be upto 50")
     @Pattern(regexp = "^[^\\s].*$", message = "Description of project cannot be empty")
     private String description;
     @NotNull(message = "start Date cannot be empty")
